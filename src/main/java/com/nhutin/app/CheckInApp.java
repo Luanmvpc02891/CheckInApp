8d1644b3fc0b9c7b106ff43d7340ecabe7dea456
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-11-09 15:55:13 +0700

    Khởi tạo dự án

--- src/main/java/com/nhutin/app/CheckInApp.java
+++ src/main/java/com/nhutin/app/CheckInApp.java
@@ -14,7 +14,7 @@ public class CheckInApp {
 
     public static void main(String[] args) {
        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+            public void run() { 
                 new Demo().setVisible(true); // MyJFrame thay bằng tên JFrame của bạn
             }
         });
