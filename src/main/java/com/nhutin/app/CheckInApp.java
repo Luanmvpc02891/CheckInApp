1b12c497c3bcfb2ad2f321a301ffda7a7cea8fe8
Mai Vũ Luân <Luanmvpc02891@gmail.com>
2023-11-09 15:48:08 +0700

    first commit

---
+++ src/main/java/com/nhutin/app/CheckInApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nhutin.app;
+
+import com.nhutin.JFrame.Demo;
+
+/**
+ *
+ * @author MaiVuLuan
+ */
+public class CheckInApp {
+
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Demo().setVisible(true); // MyJFrame thay bằng tên JFrame của bạn
+            }
+        });
+    }
+}
